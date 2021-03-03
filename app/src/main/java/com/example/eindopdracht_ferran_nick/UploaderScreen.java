47030391162fba94046ee8c4b68d06113b8b9bf8
Nick Avans <nghhoese@avans.nl>
2021-03-03 16:49:00 +0100

    backshizzle

--- app/src/main/java/com/example/eindopdracht_ferran_nick/UploaderScreen.java
+++ app/src/main/java/com/example/eindopdracht_ferran_nick/UploaderScreen.java
@@ -2,6 +2,7 @@ package com.example.eindopdracht_ferran_nick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class UploaderScreen extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class UploaderScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploader_screen);
+    }
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
